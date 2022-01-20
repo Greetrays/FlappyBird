a95141a4c4ad9685fb050e44527f3b78a6d959b6
Ренат <syltan.100renrgen@gmail.com>
2022-01-20 18:55:39 +0300

    add pool and spawned pipes

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject _container;
+    [SerializeField] private int _capacity;
+
+    [SerializeField] private List<GameObject> _pool = new List<GameObject>();
+    private Camera _camera;
+
+    protected void Initial(GameObject prefab)
+    {
+        _camera = Camera.main;
+
+        for (int i = 0; i < _capacity; i++)
+        {
+            var template = Instantiate(prefab, _container.transform);
+            template.SetActive(false);
+            _pool.Add(template);
+        }
+    }
+
+    protected bool TryGetObject(out GameObject obj)
+    {
+        obj = _pool.FirstOrDefault(p => p.activeSelf == false);
+
+        return obj != null;
+    }
+
+    protected void DisableObjectAbroadScreen()
+    {
+        Vector2 disablePoint = _camera.ViewportToWorldPoint(new Vector2(-0.5f, 0));
+
+        foreach (var item in _pool)
+        {
+            if (item.activeSelf == true)
+            {
+                if (item.transform.position.x < disablePoint.x)
+                    item.SetActive(false);
+            }
+        }
+    }
+
+    protected void Reset()
+    {
+        foreach (var item in _pool)
+        {
+            item.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PipesGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesGenerator : ObjectPool
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _minDistance;
+    [SerializeField] private float _maxDistance;
+    [SerializeField] private float _delay;
+
+    private float _elepsedTime;
+
+    private void Start()
+    {
+        Initial(_template);
+    }
+
+    private void Update()
+    {
+        _elepsedTime += Time.deltaTime;
+
+        if (_elepsedTime >= _delay)
+        {
+            if (TryGetObject(out GameObject pipe))
+            {
+                Vector3 spawnPoint = new Vector3(transform.position.x, Random.Range(_minDistance, _maxDistance), transform.position.z);
+                Spawn(pipe, spawnPoint);
+            }
+
+            _elepsedTime = 0;
+        }
+    }
+
+    private void Spawn(GameObject pipe, Vector3 spawnPoint)
+    {
+        pipe.SetActive(true);
+        pipe.transform.position = spawnPoint;
+        DisableObjectAbroadScreen();
+    }
+}
